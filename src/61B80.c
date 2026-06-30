7baa96ba8aafe9a9c11b0c54d9ad5e6dc4b3c47a
queueRAM <queueRAM@proton.me>
2026-06-30 08:48:23 +0000

    Decomp func_800629A8

--- src/61B80.c
+++ src/61B80.c
@@ -362,7 +362,10 @@ u8 func_8006291C(u16 actor_index) {
             (func_80012AB4(gActors[actor_index].posX.whole + 8, gActors[actor_index].posY.whole) != 0x36));
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/61B80/func_800629A8.s")
+u8 func_800629A8(u16 actor_index) {
+    return ((func_80012AB4(gActors[actor_index].posX.whole, gActors[actor_index].posY.whole) == 0x36) &&
+            (func_80012AB4(gActors[actor_index].posX.whole - 8, gActors[actor_index].posY.whole) != 0x36));
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/61B80/func_80062A34.s")
 

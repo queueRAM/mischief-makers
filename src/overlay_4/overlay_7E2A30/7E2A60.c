9af8c8c67e7317d5794f5843333fb054b4d3043c
queueRAM <queueRAM@proton.me>
2026-06-30 08:35:04 +0000

    Fix func_8003FF68 prototype

--- src/overlay_4/overlay_7E2A30/7E2A60.c
+++ src/overlay_4/overlay_7E2A30/7E2A60.c
@@ -15,7 +15,6 @@ extern u32 D_800D28FC;
 extern s16 D_801BD8C2_7E6A22;
 extern u32 D_80137458;
 
-extern s32 func_8003FF68(s32 arg0, f32 arg1);
 extern void func_80034D80(u16 actor_index, u16* vals); // BUG: incorrect prototype!
 extern void func_80064AA0(s32 arg0, void* arg1);
 extern s16 func_8005DEFC(void);

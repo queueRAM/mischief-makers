6099437b67dd48fedebb4ea721ed264cef4bd318
queueRAM <queueram@proton.me>
2026-06-30 01:30:43 +0000

    Decomp func_80060F80, func_80060F88

--- src/61B80.c
+++ src/61B80.c
@@ -1,8 +1,74 @@
 #include "common.h"
 
-#pragma GLOBAL_ASM("asm/nonmatchings/61B80/func_80060F80.s")
+extern s32 D_800D74A0[];
 
-#pragma GLOBAL_ASM("asm/nonmatchings/61B80/func_80060F88.s")
+void func_80060F80(s32 arg0) {
+}
+
+void func_80060F88(u16 arg0) {
+    switch (gActors[arg0].state) {
+    case 0:
+        gActors[arg0].graphicFlags |= 0x300F;
+        gActors[arg0].graphicIndex = 0;
+        gActors[arg0].scaleX = 1.0f;
+        gActors[arg0].scaleY = 1.0f;
+        gActors[arg0].unk_12C = 1.0f;
+        gActors[arg0].posZ.whole = gActors[arg0].timer_110;
+        gActors[arg0].unk_17C = D_800D74A0[gActors[arg0].unk_0D8];
+        gActors[arg0].state++;
+        if (gActors[arg0].var_154 != 0) {
+            gActors[arg0].state = gActors[arg0].var_154;
+            gActors[arg0].scaleX = 0.35f;
+            gActors[arg0].scaleY = 0.35f;
+            gActors[arg0].unk_12C = 0.35f;
+        }
+        break;
+    case 1:
+        if (gGameState == 6) {
+            if (gButtonPress & gButton_CDown) {
+                if (gActors[arg0].unk_0D8 > 0) {
+                    gActors[arg0].unk_0D8--;
+                    gActors[arg0].unk_17C = D_800D74A0[gActors[arg0].unk_0D8];
+                }
+            }
+            if (gButtonPress & gButton_CUp) {
+                if (gActors[arg0].unk_0D8 < 0x19) {
+                    gActors[arg0].unk_0D8++;
+                    gActors[arg0].unk_17C = D_800D74A0[gActors[arg0].unk_0D8];
+                }
+            }
+        }
+        if (gButtonHold & gButton_DLeft) {
+            gActors[arg0].rotateY += 4.0;
+        }
+        if (gButtonHold & gButton_DRight) {
+            gActors[arg0].rotateY -= 4.0;
+        }
+        if (gButtonHold & gButton_DDown) {
+            gActors[arg0].rotateX += 4.0;
+        }
+        if (gButtonHold & gButton_DUp) {
+            gActors[arg0].rotateX -= 4.0;
+        }
+        if (gButtonHold & gButton_LTrig) {
+            gActors[arg0].rotateZ += 4.0;
+        }
+        if (gButtonHold & gButton_RTrig) {
+            gActors[arg0].rotateZ -= 4.0;
+        }
+        break;
+    case 2:
+        gActors[arg0].rotateX += 4.0;
+        gActors[arg0].rotateY -= 4.0;
+        gActors[arg0].rotateZ -= 4.0;
+        return;
+    case 3:
+        gActors[arg0].rotateX += 4.0;
+        gActors[arg0].rotateY -= 4.0;
+        gActors[arg0].rotateZ += 4.0;
+        break;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/61B80/func_800612D8.s")
 

5373d34d9ede968b075bcfac6a3e9dcea280d83d
queueRAM <queueram@proton.me>
2026-07-03 20:37:17 +0000

    Match func_80083488

--- src/82F80.c
+++ src/82F80.c
@@ -34,11 +34,20 @@ void func_80083358(s16 arg0, s16 arg1, u16* arg2, s16 arg3) {
 
 #pragma GLOBAL_ASM("asm/nonmatchings/82F80/func_80083454.s")
 
-void func_80083488(s16, s16, s16);
-#pragma GLOBAL_ASM("asm/nonmatchings/82F80/func_80083488.s")
+void func_80083488(s16 arg0, s16 arg1) {
+    u16 index;
+    s32* var_v0;
+
+    var_v0 = (s32*)(((arg0 % 8) * 8) + ((arg0 / 8) << 0xA) + (arg1 << 0xB) + 0x8027CEE8);
+    for (index = 0; index < 0x10; index++) {
+        var_v0[0] = 0;
+        var_v0[1] = 0;
+        var_v0 += 0x10;
+    }
+}
 
 void func_80083518(s16 arg0, s16 arg1, s16 arg2, s16 arg3) {
-    u16 var_t0;
+    u16 index;
     s32* var_a0;
     s32* var_v1;
     s32 temp = arg3 * 0x44444444;
@@ -46,7 +55,7 @@ void func_80083518(s16 arg0, s16 arg1, s16 arg2, s16 arg3) {
     if (arg2 < 0x51) {
         var_v1 = (s32*)((arg2 << 6) + 0x802524A8);
         var_a0 = (s32*)(((arg0 % 16) * 4) + ((arg0 / 16) << 0xA) + (arg1 << 0xB) + 0x8027CEE8);
-        for (var_t0 = 0; var_t0 < 0x10; var_t0++) {
+        for (index = 0; index < 0x10; index++) {
             *var_a0 = *var_v1 + temp;
             var_v1 += 1;
             var_a0 += 0x10;
@@ -55,7 +64,7 @@ void func_80083518(s16 arg0, s16 arg1, s16 arg2, s16 arg3) {
     else {
         var_v1 = (s32*)((arg2 << 7) + 0x80252468);
         var_a0 = (s32*)(((arg0 % 8) * 8) + ((arg0 / 8) << 0xA) + (arg1 << 0xB) + 0x8027CEE8);
-        for (var_t0 = 0; var_t0 < 0x10; var_t0++) {
+        for (index = 0; index < 0x10; index++) {
             var_a0[0] = var_v1[0] + temp;
             var_a0[1] = var_v1[1] + temp;
             var_v1 += 2;
@@ -102,6 +111,6 @@ void func_80083878(s16 arg0, s16 arg1) {
         func_80083140(arg0, arg1);
     }
     else {
-        func_80083488(arg0, arg1 - 3, arg1);
+        func_80083488(arg0, arg1 - 3);
     }
 }

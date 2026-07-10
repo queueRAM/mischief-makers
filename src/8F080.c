8264d32ff27208349f59a95d83a76392b73d1f10
queueRAM <queueRAM@proton.me>
2026-07-10 02:34:19 +0000

    Match func_800919C8

--- src/8F080.c
+++ src/8F080.c
@@ -19,6 +19,7 @@ extern u16 D_800E8BD0[];
 extern u16 D_800E8D1C[];
 extern u16 D_800E8D3C[];
 extern u16 D_800E8D4C[];
+extern u16 D_800E8D60[];
 extern u16 D_800E8F60[];
 extern u16 D_800E8FA4[];
 extern u8 D_800E8C08[];
@@ -739,7 +740,43 @@ void func_800918D4(u16 actor_index) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/8F080/func_800919C8.s")
+void func_800919C8(u16 actor_index) {
+    u16 parent;
+    s32 index;
+
+    if (func_8008F168(actor_index) == 0) {
+        switch (gActors[actor_index].state) {
+        case 0x1A0:
+            gActors[actor_index].state++;
+            func_80081790(actor_index, D_800E8D60);
+            /* fallthrough */
+        case 0x1A1:
+            if (func_8009159C(actor_index) != 0) {
+                parent = gActors[actor_index + 7].parentIndex;
+                if (gActors[actor_index].unk_16C & 2) {
+                    gActors[actor_index].state++;
+                    gActors[parent].flags_098 &= ~ACTOR_FLAG3_UNK9;
+                    gActors[parent].flags_098 |= ACTOR_FLAG3_UNK10;
+                    if (parent == 0) {
+                        gActors[parent].unk_0F8.raw = gActors[actor_index + 2].unk_180 * gActors[actor_index + 1].unk_130;
+                        gActors[parent].unk_0FC.raw = gActors[actor_index + 3].unk_180;
+                    }
+                    else {
+                        index = gActors[parent].unk_0CE * 10;
+                        gActors[parent].unk_0F8.raw = D_800E3630[index + 4] * gActors[actor_index + 1].unk_130;
+                        gActors[parent].unk_0FC.raw = D_800E3630[index + 5];
+                    }
+                }
+            }
+            break;
+        case 0x1A2:
+            if (gActors[actor_index].unk_11C < 0.0f) {
+                gActors[actor_index].state = 0x110;
+            }
+            break;
+        }
+    }
+}
 
 void func_80091BDC(u16 actor_index);
 #pragma GLOBAL_ASM("asm/nonmatchings/8F080/func_80091BDC.s")

b51232a190b81d5fd58db04e5b18b1408009d4a0
queueRAM <queueram@proton.me>
2026-07-12 06:31:23 +0000

    Decompile 82920

--- include/80D90.h
+++ include/80D90.h
@@ -5,7 +5,7 @@
 
 void func_80080190(u16 actor_index);
 void func_8008105C(u16 actor_index, void* arg1, void* arg2);
-void func_80081478(u16 actor_index, void* arg1, s32 arg2);
+void func_80081478(u16 actor_index, s16* arg1, u16 arg2);
 void func_80081584(u16 actor_index, s16* vals, void* arg2);
 void func_80081644(u16 base_actor_index, s16* vals);
 void func_800816AC(u16 actor_index, void* arg1, s32 arg2);

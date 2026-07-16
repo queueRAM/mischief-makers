67c50c1b3e349dcc6387ce38fd5d986294fc6f68
queueRAM <queueram@proton.me>
2026-07-16 19:07:12 +0000

    Match func_8008391C

--- src/osd_debug_text.c
+++ src/osd_debug_text.c
@@ -2,9 +2,18 @@
 
 typedef struct {
     s16 unk0;
-    u8 pad2[0x68 - 2];
+    u8 unk2[0x50];
+    s32 unk54;
+    s32 unk58;
+    u8 unk5C;
+    u8 unk5D;
+    u8 unk5E;
+    u8 unk5F;
+    f32 unk60;
+    f32 unk64;
 } Unk_80180FD8; // size = 0x68
 
+extern s32 D_80180FD0;
 extern Unk_80180FD8 D_80180FD8[0x28];
 
 void func_800838E0(void) {
@@ -15,7 +24,30 @@ void func_800838E0(void) {
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/osd_debug_text/func_8008391C.s")
+void func_8008391C(u8* arg0, s32 arg1, s32 arg2, u8 arg3, u8 arg4, u8 arg5, u8 arg6, f32 arg7, f32 arg8) {
+    u16 index;
+
+    if (D_80180FD0 < 0x29) {
+        D_80180FD8[D_80180FD0].unk0 = 1;
+        D_80180FD8[D_80180FD0].unk54 = arg1;
+        D_80180FD8[D_80180FD0].unk58 = arg2;
+        D_80180FD8[D_80180FD0].unk5C = arg3;
+        D_80180FD8[D_80180FD0].unk5D = arg4;
+        D_80180FD8[D_80180FD0].unk5E = arg5;
+        D_80180FD8[D_80180FD0].unk5F = arg6;
+        D_80180FD8[D_80180FD0].unk60 = arg7;
+        D_80180FD8[D_80180FD0].unk64 = arg8;
+        
+        for (index = 0; index < 0x50; index++) {
+            D_80180FD8[D_80180FD0].unk2[index] = *arg0;
+            if (*arg0 == 0) {
+                break;
+            }
+            arg0++;
+        }
+        D_80180FD0++;
+    }
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/osd_debug_text/func_80083A04.s")
 

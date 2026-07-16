589b54f83d2cc8b9e8287dec1ea8d29638d8fd48
queueRAM <queueram@proton.me>
2026-07-16 19:11:37 +0000

    Match func_80083A04

--- src/osd_debug_text.c
+++ src/osd_debug_text.c
@@ -2,15 +2,15 @@
 
 typedef struct {
     s16 unk0;
-    u8 unk2[0x50];
-    s32 unk54;
-    s32 unk58;
-    u8 unk5C;
-    u8 unk5D;
-    u8 unk5E;
-    u8 unk5F;
-    f32 unk60;
-    f32 unk64;
+    char unk2[0x50];
+    s32 positionX;
+    s32 positionY;
+    u8 colorR;
+    u8 colorG;
+    u8 colorB;
+    u8 colorA;
+    f32 scaleX;
+    f32 scaleY;
 } Unk_80180FD8; // size = 0x68
 
 extern s32 D_80180FD0;
@@ -24,32 +24,37 @@ void func_800838E0(void) {
     }
 }
 
-void func_8008391C(u8* arg0, s32 arg1, s32 arg2, u8 arg3, u8 arg4, u8 arg5, u8 arg6, f32 arg7, f32 arg8) {
+void func_8008391C(char* str, s32 x, s32 y, u8 red, u8 green, u8 blue, u8 alpha, f32 scale_x, f32 scale_y) {
     u16 index;
 
     if (D_80180FD0 < 0x29) {
         D_80180FD8[D_80180FD0].unk0 = 1;
-        D_80180FD8[D_80180FD0].unk54 = arg1;
-        D_80180FD8[D_80180FD0].unk58 = arg2;
-        D_80180FD8[D_80180FD0].unk5C = arg3;
-        D_80180FD8[D_80180FD0].unk5D = arg4;
-        D_80180FD8[D_80180FD0].unk5E = arg5;
-        D_80180FD8[D_80180FD0].unk5F = arg6;
-        D_80180FD8[D_80180FD0].unk60 = arg7;
-        D_80180FD8[D_80180FD0].unk64 = arg8;
+        D_80180FD8[D_80180FD0].positionX = x;
+        D_80180FD8[D_80180FD0].positionY = y;
+        D_80180FD8[D_80180FD0].colorR = red;
+        D_80180FD8[D_80180FD0].colorG = green;
+        D_80180FD8[D_80180FD0].colorB = blue;
+        D_80180FD8[D_80180FD0].colorA = alpha;
+        D_80180FD8[D_80180FD0].scaleX = scale_x;
+        D_80180FD8[D_80180FD0].scaleY = scale_y;
         
         for (index = 0; index < 0x50; index++) {
-            D_80180FD8[D_80180FD0].unk2[index] = *arg0;
-            if (*arg0 == 0) {
+            D_80180FD8[D_80180FD0].unk2[index] = *str;
+            if (*str == 0) {
                 break;
             }
-            arg0++;
+            str++;
         }
         D_80180FD0++;
     }
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/osd_debug_text/func_80083A04.s")
+void func_80083A04(s32 value, s32 x, s32 y) {
+    char str[0x50];
+
+    sprintf(str, "%08X", value);
+    func_8008391C(str, x, y, gDebugOSDTint, gDebugOSDTint, gDebugOSDTint, 0xFF, 1.0f, 1.0f);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/osd_debug_text/func_80083A74.s")
 

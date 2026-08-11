40187407b7f1fad1181beccf67c2729e3e6484c3
queueRAM <queueRAM@proton.me>
2026-08-11 00:24:41 +0000

    Match func_800734C4

--- src/66250.c
+++ src/66250.c
@@ -4644,7 +4644,14 @@ u16 func_80073438(u16 actor_index) {
         gActors[actor_index].unk_184_s16[1] - 40);
 }
 
-#pragma GLOBAL_ASM("asm/nonmatchings/66250/func_800734C4.s")
+u16 func_800734C4(u16 actor_index, u16 arg1) {
+    return func_8006930C(
+        actor_index, 0xA0, 0x70, 0x210, 0x210,
+        gActors[actor_index].unk_184_s16[0] - arg1,
+        gActors[actor_index].unk_184_s16[0] + arg1,
+        gActors[actor_index].unk_184_s16[1] + 40,
+        gActors[actor_index].unk_184_s16[1] - 40);
+}
 
 #pragma GLOBAL_ASM("asm/nonmatchings/66250/func_80073558.s")
 
